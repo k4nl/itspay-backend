7ff84a0301e92adf73e98fcd530079316a037e35
gustavo braga <gustavoh.bh@gmail.com>
2023-08-17 14:45:15 -0300

    Feat - User create

--- src/services/UserServices.ts
+++ src/services/UserServices.ts
@@ -1,10 +1,27 @@
 // import User from "database/models/User";
+import Validate from "../utils/Validate";
 import CustomError from "../utils/CustomError";
 import { IUserCreate } from "interfaces/user-create.interface";
+import { PrismaClient } from "@prisma/client";
+import { User } from "../models/user.model";
+
 
 class UserService {
-  static async create(data: IUserCreate) {
-    return 'user created';
+  private static prisma = new PrismaClient();
+
+  static async findByEmail(email: string): Promise<Partial<User> | null> {
+    const response: User | null = await this.prisma.user.findUnique({ where: { email } });
+    if (!response) return null;
+
+    return { name: response.name, email: response.email, id: response.id };
+  }
+
+  static async create(userData: IUserCreate): Promise<Partial<User>> {
+    Validate.userCreateData(userData);
+    const userExists = await this.findByEmail(userData.email);
+    Validate.userAlreadyExists(userExists);
+    const response: User = await this.prisma.user.create({ data: userData });
+    return { name: response.name, email: response.email, id: response.id };
   }
 }
 
--- src/utils/Validate.ts
+++ src/utils/Validate.ts
@@ -1,1 +1,55 @@
-// file with validations
+import { IUserCreate } from "../interfaces/user-create.interface";
+import { User } from "../models/user.model";
+import CustomError from "./CustomError";
+
+export default class Validate {
+
+  static email(email: string) {
+    if(!email) {
+      throw new CustomError(400, 'Email is required');
+    }
+    if (typeof email !== 'string') {
+      throw new CustomError(400, 'Email must be a string');
+    }
+    const regex = /\S+@\S+\.\S+/;
+    if (!regex.test(email)) {
+      throw new CustomError(400, 'Invalid email');
+    }
+  }
+
+  static password(password: string) {
+    if(!password) {
+      throw new CustomError(400, 'Password is required');
+    }
+    if (typeof password !== 'string') {
+      throw new CustomError(400, 'Password must be a string');
+    }
+    if (password.length < 3) {
+      throw new CustomError(400, 'Password must be at least 3 characters');
+    }
+  }
+
+  static userName(name: string) {
+    if(!name) {
+      throw new CustomError(400, 'Name is required');
+    }
+    if (typeof name !== 'string') {
+      throw new CustomError(400, 'Name must be a string');
+    }
+    if (name.length < 3) {
+      throw new CustomError(400, 'Name must be at least 3 characters');
+    }
+  }
+
+  static userCreateData(data: IUserCreate) {
+    Validate.email(data.email);
+    Validate.password(data.password);
+    Validate.userName(data.name);
+  }
+
+  static userAlreadyExists(user: Partial<User> | null) {
+    if(user) {
+      throw new CustomError(400, 'User already exists');
+    }
+  }
+}
