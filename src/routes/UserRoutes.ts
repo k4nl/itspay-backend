c1db59e7d1b97b9d2eca989ed3e6e5e2b8632fca
gustavo braga <gustavoh.bh@gmail.com>
2023-08-18 16:59:20 -0300

    Refactoring user middleware, for checking inputs

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,14 +1,46 @@
 import express from "express";
 import UserController from "../controllers/UserController";
+import UserMiddleware from "middleware/UserMiddleware";
 import Auth from "../middleware/Auth";
 
 const router = express.Router();
 
-router.post('/login', UserController.login);
-router.post('/', UserController.create);
-router.delete('/:id', Auth.validateToken, UserController.delete);
-router.put('/:id', Auth.validateToken, UserController.update);
-router.get('/:id', Auth.validateToken, UserController.get);
-router.get('/', Auth.validateToken, UserController.getAll);
+router.post(
+  '/login',
+  UserMiddleware.login,
+  UserController.login,
+);
+
+router.post(
+  '/',
+  UserMiddleware.create,
+  UserController.create
+);
+
+router.delete(
+  '/:id',
+  Auth.validateToken,
+  UserMiddleware.delete,
+  UserController.delete
+);
+router.put(
+  '/:id',
+  Auth.validateToken,
+  UserMiddleware.update,
+  UserController.update
+);
+
+router.get(
+  '/:id',
+  Auth.validateToken,
+  UserMiddleware.findByUniqueKey,
+  UserController.get
+);
+
+router.get(
+  '/',
+  Auth.validateToken,
+  UserController.getAll
+);
 
 export default router;
